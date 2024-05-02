715d1a5844e6931c85e40ae53425ab48894188ef
Vanessa Collazos <vcollazos@hotmail.com>
2024-05-02 03:04:08 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# APP Cotizador de Criptomonedas en Tiempo Real
+
+
+## Programas utilizados:
+   React + TypeScript + Vite, base de datos a una API
+
---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,11 @@
+
+
+
+export default function ErrorMessage({children}: {children : React.ReactNode}) {
+    return (
+      <div>
+          {children}
+      </div>
+    )
+  }
+  
